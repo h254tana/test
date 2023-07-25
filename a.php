0a0a5fccf8da46e95573298e2aa8a10d80b55603
h254tana <h520254k@mails.cc.ehime-u.ac.jp>
2023-07-25 18:36:06 +0900

    a.phpを追加

--- a.php
+++ a.php
@@ -1,9 +1,9 @@
-
 #!/usr/bin/php
 <?php 
-  foreach(file('/usr/share/dict/linux.words') as $line){
-	  if(strpos($line, 'abc') !== false){
-		  echo $line;
-	  } 
-  } 
+  foreach(file('/usr/share/dict/words') as $line){
+    // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
+   if(strpos($line,"xy") !== false){
+      echo $line;
+   }
+} 
 ?>
